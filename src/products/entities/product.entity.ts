e02a592a377bbf0d44ac47964604b8b9791397f5
Joseph De La Cruz <razorij20@gmail.com>
2023-04-06 19:11:46 -0500

    agregando indexadores

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -9,7 +9,7 @@ export class Product extends Document {
   @Prop()
   description: string
 
-  @Prop({ type: Number })
+  @Prop({ type: Number, index: true }) // index: true, crea un indice para el campo
   price: number
 
   @Prop({ type: Number })
@@ -20,3 +20,4 @@ export class Product extends Document {
 }
 
 export const ProductSchema = SchemaFactory.createForClass(Product)
+ProductSchema.index({ price: 1, stock: -1 }) // 1 ascendente, -1 descendente, indeción compuesta
